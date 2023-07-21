9a4252fdb8178b51a8c4bb0aea76a9fda84e7035
DENNIS <denniskarikiu@gmail.com>
2023-07-22 00:55:37 +0300

    first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+  };
+  
+  const driversArray = ['Dan', 'Nick', 'Jim', 'Jack', 'Malkia'];
+  console.log(returnFirstTwoDrivers(driversArray));
+  console.log(returnLastTwoDrivers(driversArray));
+  console.log(selectDifferentDrivers(driversArray, returnFirstTwoDrivers));
+  console.log(selectDifferentDrivers(driversArray, returnLastTwoDrivers));
+  console.log(createFareMultiplier(4)(10));
+  console.log(fareDoubler(10));
+  console.log(fareTripler(10));
+  
